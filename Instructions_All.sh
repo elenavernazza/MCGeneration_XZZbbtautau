0f0c64874e095390520a9d7ed4667b48b791a66e
Elena Vernazza <e.vernazza1@campus.unimib.it>
2024-01-24 18:33:12 +0100

    Adding fragments and csv for ZH B2G request

--- Instructions_All.sh
+++ Instructions_All.sh
@@ -61,8 +61,6 @@ python3 batchSubmitterMC_all.py \
 --maxEvents 1000 --nJobs 150 --start_from 50 --queue reserv \
 --base /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_X_ZZbbtautau_M600
 
-####################################################################################################################################
-
 python3 batchSubmitterMC_all.py \
 --grid /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_H_quark-mass-effects_slc7_amd64_gcc700_CMSSW_10_6_37_my_gg_X_ZZbbtautau_quark-mass-effects_NNPDF31_13TeV_M700.tgz \
 --maxEvents 1000 --nJobs 150 --start_from 50 --queue reserv \
@@ -93,7 +91,19 @@ python3 batchSubmitterMC_all.py \
 --maxEvents 1000 --nJobs 150 --start_from 50 --queue long \
 --base /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_X_ZZbbtautau_M1200
 
+####################################################################################################################################
+
 python3 batchSubmitterMC_all.py \
 --grid /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_H_quark-mass-effects_slc7_amd64_gcc700_CMSSW_10_6_37_my_gg_X_ZZbbtautau_quark-mass-effects_NNPDF31_13TeV_M1300.tgz \
---maxEvents 1000 --nJobs 150 --start_from 50 --queue reserv \
+--maxEvents 1000 --nJobs 150 --start_from 50 --queue long \
 --base /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_X_ZZbbtautau_M1300
+
+python3 batchSubmitterMC_all.py \
+--grid /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_H_quark-mass-effects_slc7_amd64_gcc700_CMSSW_10_6_37_my_gg_X_ZZbbtautau_quark-mass-effects_NNPDF31_13TeV_M1400.tgz \
+--maxEvents 1000 --nJobs 150 --start_from 50 --queue reserv \
+--base /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_X_ZZbbtautau_M1400
+
+python3 batchSubmitterMC_all.py \
+--grid /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_H_quark-mass-effects_slc7_amd64_gcc700_CMSSW_10_6_37_my_gg_X_ZZbbtautau_quark-mass-effects_NNPDF31_13TeV_M1500.tgz \
+--maxEvents 1000 --nJobs 150 --start_from 50 --queue long \
+--base /data_CMS/cms/vernazza/MCProduction/2023_11_14/MyPrivateGridpacks/gg_X_ZZbbtautau_M1500
